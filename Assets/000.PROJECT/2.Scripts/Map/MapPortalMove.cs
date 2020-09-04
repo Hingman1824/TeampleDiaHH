b4d9a382398f12fcdcbabb2ed00e564cc2fe3a7c
supernine1 <supernine13@gmail.com>
2020-09-04 12:37:21 +0900

    포탈 충돌 시 선택창 활성화 -MapPortalMove 수정하고 MoveChoice 스크립트 추가 -MovePopUpCanvas 프리팹 추가

--- Assets/000.PROJECT/2.Scripts/Map/MapPortalMove.cs
+++ Assets/000.PROJECT/2.Scripts/Map/MapPortalMove.cs
@@ -6,84 +6,51 @@ using UnityEngine.UI;
 public class MapPortalMove : MonoBehaviour
 {
     public Transform PortalPoint;
-    LoadingScript loading;
     public GameObject currentMap;
     public GameObject nextMap;
     public Text mapText;
     public GameObject midCam; //미니맵 알파캠(미드)
     public GameObject zeroCam; //미니맵 알파캠(제로)
     MiniMapCamera mapName;
+    
+    //MoveChoice choice;
+    
+
+    public GameObject movePopUpCanvas;
+    
 
     public int mapNum = 0;
     //-1000,0,1000 = 은빛탑 0,0,1000 = 보스 1000,0,1000 = 2페 2000,0,1000 = 3페
     void Awake()
     {
         mapText.text = "신트리스트럼";
-        loading = FindObjectOfType<LoadingScript>();
         mapName = FindObjectOfType<MiniMapCamera>();
+        
+       // choice = FindObjectOfType<MoveChoice>();
+        movePopUpCanvas = GameObject.FindWithTag("NextMap");
     }
-    private void Update()
+
+    private void Start()
     {
-        if (CheckObjectInCamera(this.gameObject))
-        {
-            this.gameObject.transform.Rotate(Vector3.left * Time.deltaTime *5);
-        }
+        movePopUpCanvas.SetActive(false);
     }
+
     void OnTriggerEnter(Collider other) //문에 닿으면
     {
         if (other.tag == "Player") //플레이어가 문에 닿았을 때
         {
-            currentMap = nextMap;
+            movePopUpCanvas.SetActive(true);    //선택창 띄우기
+            currentMap = nextMap;       //현재 맵에 다음맵 대입
 
-            if (currentMap.name == "SilverTop")
-            {
-                mapNum = 4;
-                mapText.text = "은빛 탑";
-                midCam.transform.position = new Vector3(-1001.8f, -10f, 1013f);
-                zeroCam.transform.position = new Vector3(-1001.8f, -10f, 1013f);
-            }
-            else if (currentMap.name == "FirstCrystal")
-            {
-                mapNum = 1;
-                mapText.text = "수정회랑";
-                midCam.transform.position = new Vector3(0f, -45f, 1045f); //마커 알파값 = 3,3
-                zeroCam.transform.position = new Vector3(0f, -45f, 1045f);
-            }
-            else if(currentMap.name == "Tristrum")
-            { 
-                mapText.text = "신트리스트럼";
-                mapNum = 0;
-            }
-            else if(currentMap.name == "SecondCrystal")
-            {
-                mapNum = 2;
-                mapText.text = "공포의 영역";
-                //보스 2페이즈 미니맵 카메라 포지션
-                midCam.transform.position = new Vector3(1000f, -70f, 1000f); //마커 알파값 = 5,5
-                zeroCam.transform.position = new Vector3(1000f, -70f, 1000f); //미니맵 알파캠의 위치 변경
-            }
-            else if (currentMap.name == "LastCrystal")
-            {
-                mapNum = 3;
-                mapText.text = "수정회랑";
-                //보스 3페이즈 미니맵 카메라 포지션
-                midCam.transform.position = new Vector3(2000f, -30f, 1000f);
-                zeroCam.transform.position = new Vector3(2000f, -30f, 1000f); //미니맵 알파캠의 위치 변경
-            }
 
-            loading.Loading(); //로딩화면을 띄워주고
-
-            other.gameObject.transform.position = PortalPoint.position; //플레이어의 위치를 변경 //마커는 플레이어의 위치를 따라감
+            StartCoroutine(TransPosition(other));   //이동
         }
     }
-    public bool CheckObjectInCamera(GameObject _target) //카메라로 바라보는 화면에 오브젝트가 보여지는지 알수 있는 함수.
+
+    IEnumerator TransPosition(Collider col)     //이동
     {
-        Vector3 screenPoint = Camera.main.WorldToViewportPoint(_target.transform.position);
-        bool onScreen = screenPoint.z > 0 &&
-                        screenPoint.x > 0 &&
-                        screenPoint.x < 1 &&
-                        screenPoint.y > 0 &&
-                        screenPoint.y < 1;
-        return onScreen;
+        yield return new WaitForSeconds(5.0f);
+
+        col.gameObject.transform.position = PortalPoint.position;
     }
 }
