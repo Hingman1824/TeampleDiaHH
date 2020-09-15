a8669c16320a1281ab15adfb81fb2726d3b70810
JaeYoung97 <dhwodud97@gmail.com>
2020-09-15 21:00:57 +0900

    no message

--- Assets/000.PROJECT/2.Scripts/Map/MapPortalMove.cs
+++ Assets/000.PROJECT/2.Scripts/Map/MapPortalMove.cs
@@ -11,19 +11,19 @@ public class MapPortalMove : MonoBehaviour
     public Text mapText;
     public GameObject midCam; //미니맵 알파캠(미드)
     public GameObject zeroCam; //미니맵 알파캠(제로)
-    MiniMapCamera minimap;
+    //MiniMapCamera minimap;
     
     //MoveChoice choice;
     
 
     public GameObject movePopUpCanvas;
-    //public int mapNum;
+    public int mapNum;
     //0=마을, 1 = 1페, 2 = 2페, 3 = 3페, 4=은빛탑
     //-1000,0,1000 = 은빛탑 0,0,1000 = 보스 1000,0,1000 = 2페 2000,0,1000 = 3페
     void Awake()
     {
         mapText.text = "신트리스트럼";
-        minimap = GameObject.Find("MiniMapCamera").GetComponent<MiniMapCamera>();
+        //minimap = GameObject.Find("MiniMapCamera").GetComponent<MiniMapCamera>();
         // choice = FindObjectOfType<MoveChoice>();
         movePopUpCanvas = GameObject.FindWithTag("NextMap");
     }
@@ -50,7 +50,7 @@ public class MapPortalMove : MonoBehaviour
         yield return new WaitForSeconds(5.0f);
 
         col.gameObject.transform.position = PortalPoint.position;
-        //minimap.mapNum = mapNum;
+        MiniMapCamera.mapNum = mapNum;
         currentMap = nextMap;       //현재 맵에 다음맵 대입
     }
 }
